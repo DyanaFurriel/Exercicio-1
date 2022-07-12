5c2d5aa9bcefc05c2a433621561c2194b0375c8d
DyanaFurriel <dyanafurriel.dsf@gmail.com>
2022-07-12 11:43:08 -0300

    Exercicio1

---
+++ script.js
@@ -0,0 +1,26 @@
+// Crie uma variável que receba um nome;
+
+// Crie uma variável que receba um número;
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+let name ="Dyana";
+let number = 5;
+
+let year = 2022;
+let Age = year - number;{
+    console.log(year-number);
+}
+
+let day='quartaFeira';
+if('true'){
+    console.log('quartaFeira')
+}
